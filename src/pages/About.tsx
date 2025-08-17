026ae0c1f38394a267d96592893728b2f1edc536
Md.Abdullah al sadid <abdullahalsadid1914@gmail.com>
2025-08-17 21:29:32 +0600

    module 36

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,14 @@
 export default function About() {
   return (
-    <div>
+    <div className="py-16 px-4 container mx-auto">
       <h1>This is About Component</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, vero
+        maxime iusto quod tenetur nihil ab reiciendis totam voluptatibus ad
+        repellendus deserunt excepturi repudiandae provident voluptate vel
+        commodi! Eum accusantium ipsum soluta deserunt enim explicabo, animi
+        ipsa voluptatem facilis debitis.
+      </p>
     </div>
   );
 }
