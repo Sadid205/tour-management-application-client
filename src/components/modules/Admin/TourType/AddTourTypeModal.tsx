05205012c35923c93f0f5d381aa3cdca35e18ad0
Md.Abdullah al sadid <abdullahalsadid1914@gmail.com>
2025-08-20 17:15:23 +0600

    part-4

---
+++ src/components/modules/Admin/TourType/AddTourTypeModal.tsx
@@ -0,0 +1,77 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useAddTourTypeMutation } from "@/redux/features/Tour/tour.api";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+
+export function AddTourTypeModal() {
+  const form = useForm();
+  const [addTourType] = useAddTourTypeMutation();
+  const onSubmit = async (data) => {
+    const res = await addTourType({ name: data.name });
+    if (res?.data?.success) {
+      toast.success("Tour Type Added");
+    }
+  };
+  return (
+    <Dialog>
+      <form>
+        <DialogTrigger asChild>
+          <Button>Add Tour Type</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Add Tour Type</DialogTitle>
+          </DialogHeader>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} id="add-tour-type">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Tour Type Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Tour Type Name"
+                        {...field}
+                        value={field.value || ""}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </form>
+          </Form>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DialogClose>
+            <Button type="submit" form="add-tour-type">
+              Save changes
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </form>
+    </Dialog>
+  );
+}
--- src/redux/baseApi.ts
+++ src/redux/baseApi.ts
@@ -5,6 +5,6 @@ export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: axiosBaseQuery(),
   // baseQuery: fetchBaseQuery({baseUrl:config.baseUrl,credentials:"include"})
-  tagTypes: ["USER"],
+  tagTypes: ["USER", "TOUR"],
   endpoints: () => ({}),
 });
