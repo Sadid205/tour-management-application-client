29a2d372e3133aebe00891f58f693b72130251d2
Md.Abdullah al sadid <abdullahalsadid1914@gmail.com>
2025-08-22 17:19:13 +0600

    39-9

--- src/pages/Admin/AddTour.tsx
+++ src/pages/Admin/AddTour.tsx
@@ -1,4 +1,6 @@
+import MultipleImageUploader from "@/components/MultipleImageUploader";
 import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
 import {
   Card,
   CardAction,
@@ -11,14 +13,13 @@ import {
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { Popover } from "@/components/ui/popover";
 import {
   Select,
   SelectContent,
@@ -27,17 +28,28 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
+import type { FileMetadata } from "@/hooks/use-file-upload";
+import { cn } from "@/lib/utils";
 import { useGetDivisionQuery } from "@/redux/features/division/division.api";
-import { useGetTourTypesQuery } from "@/redux/features/tour/tour.api";
+import {
+  useAddTourMutation,
+  useGetTourTypesQuery,
+} from "@/redux/features/tour/tour.api";
+import { PopoverContent, PopoverTrigger } from "@radix-ui/react-popover";
+import { format, formatISO } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+import { useState } from "react";
+
 import { useForm, type FieldValues, type SubmitHandler } from "react-hook-form";
-import { Link } from "react-router";
 
 export default function AddTour() {
+  const [images, setImages] = useState<(File | FileMetadata)[] | []>([]);
+  console.log(images);
   const { data: tourTypeData, isLoading: tourTypeLoading } =
     useGetTourTypesQuery(undefined);
   const { data: divisionData, isLoading: divisionLoading } =
     useGetDivisionQuery(undefined);
-
+  const [addTour] = useAddTourMutation();
   const divisionOptions = divisionData?.map(
     (item: { _id: string; name: string }) => ({
       value: item._id,
@@ -57,10 +69,25 @@ export default function AddTour() {
       division: "",
       tourType: "",
       description: "",
+      startDate: "",
+      endDate: "",
     },
   });
   const handleSubmit: SubmitHandler<FieldValues> = async (data) => {
-    console.log(data);
+    const tourData = {
+      ...data,
+      startDate: formatISO(data.startDate),
+      endDate: formatISO(data.endDate),
+    };
+    const formData = new FormData();
+    formData.append("data", JSON.stringify(tourData));
+    images.forEach((image) => formData.append("files", image as File));
+    try {
+      const res = await addTour(formData).unwrap();
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
   };
   return (
     <div className="w-full max-w-4xl mx-auto px-5 mt-16">
@@ -95,7 +122,7 @@ export default function AddTour() {
                   name="division"
                   render={({ field }) => (
                     <FormItem className="flex-1">
-                      <FormLabel>Tour Type</FormLabel>
+                      <FormLabel>Division</FormLabel>
                       <Select
                         onValueChange={field.onChange}
                         defaultValue={field.value}
@@ -125,7 +152,7 @@ export default function AddTour() {
                   name="tourType"
                   render={({ field }) => (
                     <FormItem className="flex-1">
-                      <FormLabel>Email</FormLabel>
+                      <FormLabel>Tour Type</FormLabel>
                       <Select
                         onValueChange={field.onChange}
                         defaultValue={field.value}
@@ -151,24 +178,125 @@ export default function AddTour() {
                   )}
                 />
               </div>
-              <FormField
-                control={form.control}
-                name="description"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Description</FormLabel>
-                    <FormControl>
-                      <Textarea {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+              <div className="flex gap-5">
+                <FormField
+                  control={form.control}
+                  name="startDate"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-1 flex-col">
+                      <FormLabel>Start Date</FormLabel>
+                      <Popover>
+                        <PopoverTrigger asChild>
+                          <FormControl>
+                            <Button
+                              variant={"outline"}
+                              className={cn(
+                                "w-full pl-3 text-left font-normal",
+                                !field.value && "text-muted-foreground"
+                              )}
+                            >
+                              {field.value ? (
+                                format(field.value, "PPP")
+                              ) : (
+                                <span>Pick a date</span>
+                              )}
+                              <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                            </Button>
+                          </FormControl>
+                        </PopoverTrigger>
+                        <PopoverContent
+                          className="w-auto p-0 bg-muted"
+                          align="start"
+                        >
+                          <Calendar
+                            mode="single"
+                            selected={new Date(field.value)}
+                            onSelect={field.onChange}
+                            captionLayout="dropdown"
+                            disabled={(date) =>
+                              date <
+                              new Date(
+                                new Date().setDate(new Date().getDate() - 1)
+                              )
+                            }
+                          />
+                        </PopoverContent>
+                      </Popover>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="endDate"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-1 flex-col">
+                      <FormLabel>End Date</FormLabel>
+                      <Popover>
+                        <PopoverTrigger asChild>
+                          <FormControl>
+                            <Button
+                              variant={"outline"}
+                              className={cn(
+                                "w-full pl-3 text-left font-normal",
+                                !field.value && "text-muted-foreground"
+                              )}
+                            >
+                              {field.value ? (
+                                format(field.value, "PPP")
+                              ) : (
+                                <span>Pick a date</span>
+                              )}
+                              <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                            </Button>
+                          </FormControl>
+                        </PopoverTrigger>
+                        <PopoverContent
+                          className="w-auto p-0 bg-muted"
+                          align="start"
+                        >
+                          <Calendar
+                            mode="single"
+                            selected={new Date(field.value)}
+                            onSelect={field.onChange}
+                            disabled={(date) =>
+                              date <
+                              new Date(
+                                new Date().setDate(new Date().getDate() - 1)
+                              )
+                            }
+                            captionLayout="dropdown"
+                          />
+                        </PopoverContent>
+                      </Popover>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className="flex gap-5 items-stretch">
+                <FormField
+                  control={form.control}
+                  name="description"
+                  render={({ field }) => (
+                    <FormItem className="flex-1">
+                      <FormLabel>Description</FormLabel>
+                      <FormControl>
+                        <Textarea {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <div className="flex-1">
+                  <MultipleImageUploader onChange={setImages} />
+                </div>
+              </div>
             </form>
           </Form>
         </CardContent>
         <CardFooter className="flex justify-end">
-          <Button type="submit" className="">
+          <Button type="submit" form="add-tour-form" className="">
             Create Tour
           </Button>
         </CardFooter>
